ae76084e349118baaeb74c9e238b4b5e816a4b06
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-04-01 17:35:43 +0300

    feat : add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\NftController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->apiResources(
+    [
+        'nft' => NftController::class
+    ]
+);
+
+Route::post('/login', [AuthController::class, 'login']);
