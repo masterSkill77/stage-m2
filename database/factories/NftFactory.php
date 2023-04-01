ea5e06a2dc1760db951a2de8f810ae063bd6df75
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-04-01 16:16:50 +0300

    feat : seeder

---
+++ database/factories/NftFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Nft>
+ */
+class NftFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->title(),
+            'description' => $this->faker->paragraph(1),
+            'image_uri' => $this->faker->url(),
+            'owner_id' => 1
+        ];
+    }
+}
