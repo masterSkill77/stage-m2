045dadc8f657b4d3e7183f25f50ea2fe35c89db9
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-04-24 15:34:02 +0300

    fix / change complet url

--- resources/views/vendor/notification/new-bid.blade.php
+++ resources/views/vendor/notification/new-bid.blade.php
@@ -3,7 +3,7 @@
       <h1 style="color: #333; margin-bottom: 20px;"><span>Hurry up </span>{{ $user->name }}</h1>
       <img src="https://img.freepik.com/vecteurs-libre/bulle-dialogue-texte-wow-style-pop-art_603843-1001.jpg?t=st=1680545896~exp=1680546496~hmac=271b0457044f4035fb1f30c8863915664e6789178dcb4bdcff284b5d542a7447" style="max-width: 100%; height: auto; display: block; margin: 0 auto; margin-bottom: 20px;">
       <p style="font-size: 18px; line-height: 1.5;">Someone just make a new bid on the <b>{{ $auction->nft->title }}</b> item</p>
-      <p style="font-size: 18px; line-height: 1.5;"><span>To see this item, <a href="<?= env("URL_CLIENT") . '/auction/' . $auction->nft->id ?>">check out here</a></span></p>
+      <p style="font-size: 18px; line-height: 1.5;"><span>To see this item, <a href="<?= env("URL_CLIENT") . '/auction/' . $auction->id ?>">check out here</a></span></p>
     </div>
   </div>
 </div>
