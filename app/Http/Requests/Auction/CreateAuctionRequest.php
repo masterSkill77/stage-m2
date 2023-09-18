4068684f538e29690ecd29350ab594829ac15a7b
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-09-18 22:41:25 +0300

    feat : add eth paiement

--- app/Http/Requests/Auction/CreateAuctionRequest.php
+++ app/Http/Requests/Auction/CreateAuctionRequest.php
@@ -24,7 +24,7 @@ class CreateAuctionRequest extends FormRequest
      */
     public function rules(): array
     {
-        return ['end_date' => 'bail|required', 'start_date' => 'required', 'current_bid', 'start_price' => 'numeric', 'nft_id' => 'required'];
+        return ['end_date' => 'bail|required', 'start_date' => 'required', 'current_bid', 'start_price' => 'numeric', 'nft_id' => 'required', 'payment' => 'required|string'];
     }
     protected function failedValidation(Validator $validator)
     {
